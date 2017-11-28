4e24cdf8aba9094442ee948048daef30cbbe0f36
rabzu <z.kakabadze@gmail.com>
2017-11-28 16:04:52 +0000

    Print connection pool when its created. Change blob upload to chunked and remove akka-http upload size limit. bump 0.1.1

--- src/main/scala/io/pixelart/ambry/client/infrastructure/adapter/akkahttp/RequestsPoolExecutor.scala
+++ src/main/scala/io/pixelart/ambry/client/infrastructure/adapter/akkahttp/RequestsPoolExecutor.scala
@@ -2,24 +2,27 @@ package io.pixelart.ambry.client.infrastructure.adapter.akkahttp
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
-import akka.stream.scaladsl.{ Keep, Sink, Source }
-import akka.stream.{ ActorMaterializer, OverflowStrategy, QueueOfferResult, StreamTcpException }
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult, StreamTcpException}
 import com.typesafe.scalalogging.StrictLogging
 import io.pixelart.ambry.client.domain.model.AmbryHttpConnectionException
-import scala.concurrent.{ ExecutionContext, Future, Promise }
-import scala.util.{ Failure, Success }
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success, Try}
 
 class RequestsPoolExecutor(host: String, port: Int = 1174)(implicit val actorSystem: ActorSystem, val executionContext: ExecutionContext, val materializer: ActorMaterializer)
     extends AkkaHttpAmbryResponseHandler
     with StrictLogging {
-//todo: Move everythign to Akka URI
-  private lazy val poolFlow = Http().cachedHostConnectionPool[Promise[HttpResponse]](host.split("http[s]?://").tail.head, port)
+
+  //todo: Move everythign to Akka URI
+  private lazy val poolFlow:  Flow[(HttpRequest, Promise[HttpResponse]), (Try[HttpResponse], Promise[HttpResponse]), Http.HostConnectionPool] =
+    Http().cachedHostConnectionPool[Promise[HttpResponse]](host.split("http[s]?://").tail.head, port)
 
   private lazy val queueSize = 50
 
-  private lazy val (queueSource, poolFuture) = Source.queue[(HttpRequest, Promise[HttpResponse])](queueSize, OverflowStrategy.backpressure)
-    .via(poolFlow)
+  private val (queueSource, connectionPool) = Source.queue[(HttpRequest, Promise[HttpResponse])](queueSize, OverflowStrategy.backpressure)
+    .viaMat(poolFlow)(Keep.both)
     .toMat(
       Sink.foreach({
         case ((Success(resp), p)) =>
@@ -27,19 +30,10 @@ class RequestsPoolExecutor(host: String, port: Int = 1174)(implicit val actorSys
           p.success(resp)
         case ((Failure(e), p)) => p.failure(e)
       })
-    )(Keep.both)
+    )(Keep.left)
     .run()
 
-
-  poolFuture.onComplete{
-      case Success(_) =>
-        logger.debug("ambry/connection/pool/success/closed ")
-      case Failure(e) =>
-        logger.warn("ambry/request/failure/message={}", e.getMessage)
-        e.printStackTrace()
-    }
-
-
+  logger.info("ambry/connectionPool/setting={}/host={}/port={}",connectionPool.setup.toString)
 
   protected[akkahttp] def executeRequest[T](httpRequest: HttpRequest, unmarshal: HttpResponse => Future[T]): Future[T] = {
 
